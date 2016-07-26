0551f41d452d9e601db8ada56252653ceadd068d
Olga Vyrostko <violg@37.140.186.17-ekb.dhcp.yndx.net>
2016-07-26 14:15:18 +0500

    gitignore

---
+++ Slider/Classes/Slider.h
@@ -0,0 +1,13 @@
+//
+//  Slider.h
+//  Pods
+//
+//  Created by Olga Vyrostko on 26.07.16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Slider : UIImage
+
+@end
---
+++ Slider/Classes/Slider.m
@@ -0,0 +1,13 @@
+//
+//  Slider.m
+//  Pods
+//
+//  Created by Olga Vyrostko on 26.07.16.
+//
+//
+
+#import "Slider.h"
+
+@implementation Slider
+
+@end
